e4a70dfbd12837506926c311493c16457dffe9f0
Danilson Sanches <dsanches06@outlook.com>
2020-03-31 15:39:11 +0100

    Obtendo um usuário por id

---
+++ src/main/java/com/project/springboot/mongodb/resources/exception/ResourceExceptionHandler.java
@@ -0,0 +1,25 @@
+package com.project.springboot.mongodb.resources.exception;
+
+import java.time.Instant;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.project.springboot.mongodb.services.exception.ObjectNotFoundException;
+
+@ControllerAdvice
+public class ResourceExceptionHandler {
+
+	@ExceptionHandler(ObjectNotFoundException.class)
+	public ResponseEntity<StandardError> objectNotFoud(ObjectNotFoundException e, HttpServletRequest request) {
+		String error = "Not found";
+		HttpStatus status = HttpStatus.NOT_FOUND;
+		StandardError standardError = new StandardError(Instant.now(), status.value(), error, e.getMessage(),
+				request.getRequestURI());
+		return ResponseEntity.status(status).body(standardError);
+	}
+}
--- src/main/java/com/project/springboot/mongodb/services/UserService.java
+++ src/main/java/com/project/springboot/mongodb/services/UserService.java
@@ -1,12 +1,14 @@
 package com.project.springboot.mongodb.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.project.springboot.mongodb.domain.User;
 import com.project.springboot.mongodb.repository.UserRepository;
+import com.project.springboot.mongodb.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,5 +18,10 @@ public class UserService {
 
 	public List<User> findAll() {
 		return repository.findAll();
+	}
+
+	public User findById(String id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Object not found"));
 	}
 }
